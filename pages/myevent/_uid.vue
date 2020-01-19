e860e2c31761130a5b1d3b8f538b6f4ac5e75f8a
yutoc11 <kikuchi-yuto@sagojo.link>
2020-01-19 22:53:16 +0900

    myeventのページのURLを読み取る仕組みをテスト

---
+++ pages/myevent/_uid.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  section.section
+    .test ここはユーザーごとのページ
+    .test リンクが動的に表示される
+    p {{ $route.params.uid }}
+
+</template>
+
+<script>
+
+import firebase from '@/plugins/firebase'
+import { mapActions, mapState, mapGetters } from 'vuex'
+
+
+export default {
+
+  head() {
+    return {
+      title:'マイページ'
+    }
+  },
+
+  data () {
+    return {
+      tabs: null,
+      newEmail: '',
+      currentPassword: '',
+      currentPasswordforEmail: '',
+      newPassword: '',
+      newPasswordConfirm: '',
+      title: 'マイページ'
+    };
+  },
+
+  asyncData () {
+    return {
+    }
+  },
+
+  components: {
+  },
+
+  computed: {
+
+  },
+
+  created: function(){
+  },
+
+  mounted: function () {
+  },
+
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="scss">
+.section{
+  margin: 0 auto;
+}
+
+</style>
